d960a97850adcacb8ed2450756febe5240b38146
jlngomes <jean.luca162021@outlook.com>
2026-08-03 22:38:29 -0300

    :sparkles: feat: implement linked list print

--- data-structures/python/linked_list.py
+++ data-structures/python/linked_list.py
@@ -10,3 +10,9 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
