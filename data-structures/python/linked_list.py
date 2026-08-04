8ff2b5b99e8117461f0d6dce2de8a83982043570
jlngomes <jean.luca162021@outlook.com>
2026-08-03 22:39:22 -0300

    :sparkles: feat: implement linked list append

--- data-structures/python/linked_list.py
+++ data-structures/python/linked_list.py
@@ -16,3 +16,19 @@ class LinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+
+    def append(self, value):
+        """
+        1. Create new Node
+        2. add Node to end
+        """
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
